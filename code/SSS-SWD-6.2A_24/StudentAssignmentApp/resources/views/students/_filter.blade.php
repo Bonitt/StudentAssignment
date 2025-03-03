b0f1fa03fd637c344e62afd58428b8258c5eced6
Bonitt <matthewbonett1309@gmail.com>
2025-03-03 13:14:59 +0000

    added non functional filter

---
+++ code/SSS-SWD-6.2A_24/StudentAssignmentApp/resources/views/students/_filter.blade.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-md-6"></div>
+    <div class="col-md-6">
+        <div class="row">
+            <div class="col">
+                <div class="input-group mb-3">
+                    <select class="custom-select">
+                        <option value="" selected>All Colleges</option>
+                        <option value="1">College One</option>
+                        <option value="2">College Two</option>
+                        <option value="3">College Three</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
