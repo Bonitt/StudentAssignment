c111a77bef834c6c0b2471fd0b50632a7e059a14
Bonitt <matthewbonett1309@gmail.com>
2025-03-03 12:46:18 +0000

    Added content to main and added the extends layout

--- code/SSS-SWD-6.2A_24/StudentAssignmentApp/resources/views/students/index.blade.php
+++ code/SSS-SWD-6.2A_24/StudentAssignmentApp/resources/views/students/index.blade.php
@@ -1,3 +1,10 @@
+@extends('layouts.main');
+
+@section('content')
+
+
 <h1>All Students</h1>
 <a href="{{ route('students.create') }}">Create New Student</a>
 <a href="{{ route('students.show', 1) }}">Show Student</a>
+
+@endsection
