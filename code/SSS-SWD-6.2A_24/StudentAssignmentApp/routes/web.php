2b0eda1d9b796bb4f11affebc41ffa3d4ba393a5
Bonitt <matthewbonett1309@gmail.com>
2025-03-03 09:25:57 +0000

    Added Routes (to be changed)

--- code/SSS-SWD-6.2A_24/StudentAssignmentApp/routes/web.php
+++ code/SSS-SWD-6.2A_24/StudentAssignmentApp/routes/web.php
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/colleges', function () {
+    return "<h1>All Colleges</h1>";
+})->name('colleges.index');
+
+Route::get('/colleges/create', function () {
+    return "<h1>Add new college</h1>";
+})->name('colleges.create');
+
+Route::get('/colleges/{id}/edit', function ($id) {
+    return "<h1>Edit college with id: $id</h1>";
+})->name('colleges.edit');
+
+Route::get('/students', function(){
+    return "<h1>All Students</h1>";
+})->name('students.index');
+
+Route::get('/students/create', function () {
+    return "<h1>Add new student</h1>";
+})->name('students.create');
+
+Route::get('/students/{id}/edit', function ($id) {
+    return "<h1>Edit student with id: $id</h1>";
+})->name('students.edit');
+
