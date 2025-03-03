d8db1a58cbf7df9d5ad729a0832d3e6d73570b56
Bonitt <matthewbonett1309@gmail.com>
2025-03-03 08:11:29 +0000

    moved cardealer

--- code/SSS-SWD-6.2A_24/cardealerapp/public/app.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('filter_device_id').addEventListener('change', 
-    function() {
-        let deviceId = this.value || this.options[this.selectedIndex].value;
-        window.location.href = window.location.href.split('?')[0] + '?device_id=' + deviceId;
-    }
-);
